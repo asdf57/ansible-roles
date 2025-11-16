8c3e0612f6d6fbfd1d4f0677673d7eda846f5112
asdf57 <53836417+asdf57@users.noreply.github.com>
2025-11-15 22:05:57 -0500

    Try setting CONCOURSE_CONTAINERD_ALLOW_HOST_ACCESS instead of forced iptables delete

--- roles/init/files/concourse/entrypoint.sh
+++ roles/init/files/concourse/entrypoint.sh
@@ -14,16 +14,4 @@ export CONCOURSE_VAULT_AUTH_PARAM="role_id:$role_id,secret_id:$secret_id"
 
 echo "$CONCOURSE_VAULT_AUTH_PARAM"
 
-cleanup_iptables_rule() {
-    if iptables -C INPUT -i concourse0 -j REJECT --reject-with icmp-host-prohibited &>/dev/null; then
-        echo "Detected and removing unwanted iptables rule for concourse0."
-        iptables -D INPUT -i concourse0 -j REJECT --reject-with icmp-host-prohibited
-    fi
-}
-
-(while true; do
-    cleanup_iptables_rule
-    sleep 5
-done) &
-
 exec dumb-init /usr/local/bin/entrypoint.sh "$@"
